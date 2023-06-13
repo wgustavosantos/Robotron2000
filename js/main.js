1ff708aa943ae232ac74b33a5b95c94d15f13d37
Wenderson Gustavo Santos <guto15santos@gmail.com>
2023-06-12 22:04:00 -0300

    Funções com JavaScript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.querySelector('#robotron').addEventListener('click', (e) => {console.log(e)});
+
+function dizOi() {
+    console.log("oi");
+}
+
+function dizOiComParametro(nome){
+    console.log("Oi, " + nome + "! Bem vindo.")
+}
+
+
