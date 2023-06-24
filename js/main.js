9efb0654d27cc21f32cd78cd9b63071e8c99cbb3
Wenderson Gustavo Santos <guto15santos@gmail.com>
2023-06-23 22:04:00 -0300

    Reaproveitando código

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
+function alterar(e) {
+
+    if (e.target.id === 'somar') {
+        braco.value++;
+    } else if (e.target.id === 'subtrair') {
+        braco.value--;
+    }
+}
+
 var soma = document.querySelector('#somar');
 var subtracao = document.querySelector('#subtrair');
 var braco = document.querySelector('#braco');
+var controle = document.querySelectorAll('.peca > .controle > .controle-ajuste');
 
-soma.addEventListener('click', () => {
-    braco.value++;
-})
-
-subtracao.addEventListener('click', () => {
-    braco.value--;
+controle.forEach((e) => {
+    e.addEventListener('click', (evento) => {
+        alterar(evento)
+    })
 })
